b17d251d9deb18fa099f6696b4622ac67ada1c3b
sofiagarcia358 <159475085+sofiagarcia358@users.noreply.github.com>
2025-01-14 17:25:26 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+//variables
+
+let edad = 18;
+let nombre = "sofia";
+let libras = 1.5;
+
+const PI = 3.1416;
+
+//Ejercicio
+let alumnos = 21;
+let clases = 50;
+let asistencia = true;
+let peso_compu = 6.5;
+let horario = 6.45;
+let recreo = true;
+let cordinadora = "Flor";
+let Directora = "Mayra";
+let edades = 17;
+let aparato_de_Estudio = "computación";
+
+
+//tarea
+//1
+let tiempo = 10;
+let vf = 30;
+let vi = 0;
+
+//2
+let lote = 150;
+let costoCOMPRA = 50;
+let venta = 75;
